309ea6122a55e86ceb11d3f6fb9d3d54fa59f45b
Ignacio <ignacionazar9@gmail.com>
2025-07-11 00:45:12 -0300

    agregue la estructura del ejercicio 2

---
+++ Informacion.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Informacion = () => {
+    return <span> my friend!</span>;
+}
+
+export default Informacion;
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Informacion from "../Informacion";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <h1 className="text-center my-5">
+      Hello
+      <Informacion></Informacion>
+    </h1>
+  );
 }
 
-export default App
+export default App;
